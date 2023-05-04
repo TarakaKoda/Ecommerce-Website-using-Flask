8e0ae8c69abf2f0ad65f5ea95971ce1f09a2ac0b
TarakaKoda <taraka.koda@gmail.com>
2023-05-05 00:30:49 +0530

    created register page route

--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for
-from market.models import Item
+from flask import render_template, url_for, redirect
+from market.models import Item, User
 from market import app
-
-
+from market.forms import RegisterForm
+from market import db
 
 @app.route("/")
 @app.route("/home")
@@ -14,4 +14,20 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items, title="Market Page")
+
+
+@app.route("/register", methods=["GET","POST"])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+    if form.errors != {}: # if there are not error from the validations
+        for err_mgs in form.errors.values():
+            print(f"There was an error with creating a user: {err_mgs}")
+    return render_template("register.html", form=form, title="Register Page")
+
+
 
