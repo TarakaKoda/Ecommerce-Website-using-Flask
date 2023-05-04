a281d63926042dfbf9dd16f33a7107baf487fd8e
TarakaKoda <taraka.koda@gmail.com>
2023-05-05 00:28:39 +0530

    config secret key

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "e88f4c0d5e3eb689ced253da"
 db = SQLAlchemy(app)
 
 from market import routes
