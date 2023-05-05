5fc77d31572c6ba4f71ccc51f847b8bcd9cc82cc
TarakaKoda <taraka.koda@gmail.com>
2023-05-06 02:09:46 +0530

    created LoginForm, PurchasedItemForm, SellItemForm

--- market/forms.py
+++ market/forms.py
@@ -24,3 +24,16 @@ class RegisterForm(FlaskForm):
     password2 = PasswordField(label="Confirm Password", validators=[EqualTo("password1"), DataRequired()])
     submit = SubmitField(label="Create Account")
 
+
+class LoginForm(FlaskForm):
+    username = StringField(label="User Name", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="Sign In")
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label="Purchase Item!")
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label="Sell Item!")
+
